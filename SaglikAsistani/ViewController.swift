0e1fd19b5d786bed6feddb761282a8a36add7986
Burak Furkan Asiltürk <asilturk@burak-furkan>
2018-07-04 23:57:28 +0300

    Initial Commit

---
+++ SaglikAsistani/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SaglikAsistani
+//
+//  Created by Burak Furkan Asilturk on 4.07.2018.
+//  Copyright © 2018 Burak Furkan Asilturk. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
