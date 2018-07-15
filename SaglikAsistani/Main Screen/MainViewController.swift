926d95542251c01fb5d7c3891e7d1cd1cf5d73a3
Burak Furkan Asiltürk <asilturk@burak-furkan>
2018-07-15 12:21:35 +0300

    sign out button created

--- SaglikAsistani/Main Screen/MainViewController.swift
+++ SaglikAsistani/Main Screen/MainViewController.swift
@@ -21,14 +21,36 @@ class MainViewController: UIViewController, WKNavigationDelegate {
         DispatchQueue.main.async {
             self.webView = WKWebView()
             self.webView.navigationDelegate = self
+            self.webView.scrollView.isScrollEnabled = false
             self.view = self.webView
+            self.signOutButtonConfiguration()
             self.startWebView()
+            
         }
     }
 }
 
 // MARK: - Auxiliary Methods
 extension MainViewController {
+    
+    fileprivate func signOutButtonConfiguration() {
+        let frame = CGRect.init(x: 0, y: 0, width: 30, height: 30)
+        let logoutButton = UIButton.init(frame: frame)
+        logoutButton.setImage(#imageLiteral(resourceName: "logout-icon"), for: UIControlState.normal)
+        
+        self.view.addSubview(logoutButton)
+      
+        logoutButton.translatesAutoresizingMaskIntoConstraints = false
+        
+//        let horizontalConstraint = NSLayoutConstraint(item: logoutButton, attribute: NSLayoutAttribute.centerX, relatedBy: NSLayoutRelation.equal, toItem: view, attribute: NSLayoutAttribute.centerX, multiplier: 1, constant: 0)
+//        let verticalConstraint = NSLayoutConstraint(item: logoutButton, attribute: NSLayoutAttribute.centerY, relatedBy: NSLayoutRelation.equal, toItem: view, attribute: NSLayoutAttribute.centerY, multiplier: 1, constant: 0)
+        let trailingConstraint = NSLayoutConstraint(item: logoutButton, attribute: NSLayoutAttribute.trailing, relatedBy: NSLayoutRelation.equal, toItem: view, attribute: NSLayoutAttribute.trailing, multiplier: 1, constant: 16)
+        let topConstraint = NSLayoutConstraint(item: logoutButton, attribute: NSLayoutAttribute.top, relatedBy: NSLayoutRelation.equal, toItem: view, attribute: NSLayoutAttribute.top, multiplier: 1, constant: -10)
+        let widthConstraint = NSLayoutConstraint(item: logoutButton, attribute: NSLayoutAttribute.width, relatedBy: NSLayoutRelation.equal, toItem: nil, attribute: NSLayoutAttribute.notAnAttribute, multiplier: 1, constant: 100)
+        let heightConstraint = NSLayoutConstraint(item: logoutButton, attribute: NSLayoutAttribute.height, relatedBy: NSLayoutRelation.equal, toItem: nil, attribute: NSLayoutAttribute.notAnAttribute, multiplier: 1, constant: 100)
+        view.addConstraints([trailingConstraint, topConstraint, widthConstraint, heightConstraint])
+
+    }
     
     fileprivate func startWebView() {
         
